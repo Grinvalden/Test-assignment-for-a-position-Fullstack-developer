4d189c7b27142439b656c31a9eb98e75e3d058e5
Grinvalden <1515@dr.com>
2024-11-12 20:48:07 +0300

    added checking what countSymbols isn't negative

--- task2/index.php
+++ task2/index.php
@@ -2,31 +2,32 @@
 
 class parseText 
 {
-public string $str;
+    public string $str;
 
-public function __construct($str = '') 
+    public function __construct($str = '') 
     {
         $this->str = $str;
     }
 
-public function checkStr() 
+    public function checkStr() 
     {
         $countSymbols = 0;
         $arraySymbols = str_split($this->str);
+
         foreach ($arraySymbols as $i) {
             if ($i === "{") {
                 $countSymbols += 1;
-            }
-            elseif ($i === "}") {
+            } elseif ($i === "}") {
                 $countSymbols -= 1;
             }
+
+            if ($countSymbols < 0) {
+                return "Строка некорректна";
+            }
         }
-        $result = ($countSymbols === 0) ? "Строка корректна" : "Строка некорректна";
-        return $result;
 
+        return ($countSymbols === 0) ? "Строка корректна" : "Строка некорректна";
     }
-
-
 }
 
 ?>
